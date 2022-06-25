dcd7c329459fe6baf5c3d652f61aba9ba1d11950
viscropst <bcct2010@live.cn>
2022-06-25 12:56:12 +0800

    core/cmd: add 'arg-str' option and warning deletion

--- core/cmd.go
+++ core/cmd.go
@@ -6,12 +6,28 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 )
 
+type splitString struct {
+	SourceStr string `toml:"src"`
+	SplitStr  string `toml:"split-str"`
+	SplitNum  int8   `toml:"split-num"`
+}
+
+func (sp *splitString) ToArray() []string {
+	sp.SourceStr = strings.ReplaceAll(sp.SourceStr, "\r\n", "")
+	if sp.SplitNum != 0 {
+		return strings.SplitN(sp.SourceStr, sp.SplitStr, int(sp.SplitNum))
+	}
+	return strings.Split(sp.SourceStr, sp.SplitStr)
+}
+
 type Command struct {
-	cmd  exec.Cmd
-	Exec string   `toml:"exec"`
-	Args []string `toml:"args"`
+	cmd    exec.Cmd
+	Exec   string      `toml:"exec"`
+	Args   []string    `toml:"args"`
+	ArgStr splitString `toml:"arg-str"`
 	internal.ValueScope
 	WithEnviron bool `toml:"with-environ"`
 }
@@ -36,10 +52,14 @@ func (c *Command) Prepare(vars ...map[string]string) *Command {
 		c.Args[idx] = c.Expand(c.Args[idx])
 	}
 	c.Exec = c.Expand(c.Exec)
+	if c.ArgStr.SourceStr != "" {
+		c.ArgStr.SourceStr = c.Expand(c.ArgStr.SourceStr)
+		c.Args = append(c.Args, c.ArgStr.ToArray()...)
+	}
 	return c
 }
 
-func (c *Command) prepareCmd(vars ...map[string]string) (*exec.Cmd, error) {
+func (c *Command) prepareCmd() (*exec.Cmd, error) {
 	cmd := exec.Cmd{
 		Stdin:  c.cmd.Stdin,
 		Stdout: c.cmd.Stdout,
@@ -48,9 +68,9 @@ func (c *Command) prepareCmd(vars ...map[string]string) (*exec.Cmd, error) {
 	cmd.Path = c.Exec
 	if filepath.Base(c.Exec) == c.Exec {
 		var err0 error
-		os.Setenv("PATH", c.Env["PATH"])
+		_ = os.Setenv("PATH", c.Env["PATH"])
 		cmd.Path, err0 = exec.LookPath(c.Exec)
-		os.Unsetenv("PATH")
+		_ = os.Unsetenv("PATH")
 		if err0 != nil {
 			return nil, err0
 		}
@@ -68,7 +88,7 @@ func (c *Command) prepareCmd(vars ...map[string]string) (*exec.Cmd, error) {
 
 func (c *Command) Execute(vars ...map[string]string) error {
 	c.Prepare(vars...)
-	cmd, err := c.prepareCmd(vars...)
+	cmd, err := c.prepareCmd()
 	if err != nil {
 		return err
 	}
@@ -81,7 +101,7 @@ func (c *Command) Execute(vars ...map[string]string) error {
 
 func (c *Command) ExecuteNoWait(vars ...map[string]string) error {
 	c.Prepare(vars...)
-	cmd, err := c.prepareCmd(vars...)
+	cmd, err := c.prepareCmd()
 	if err != nil {
 		return err
 	}
