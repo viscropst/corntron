931905f35634c9f9b7186cf1ce113d1317ed7642
viscrop <viscropst@petalmail.com>
2025-05-05 15:07:41 +0800

    coding-ci: add pushing stages for coding ci

---
+++ .build/build-binaries.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+go env -w GOPROXY=https://goproxy.cn,direct
+go mod tidy
+out_dir=$PWD/out
+mkdir -p ${out_dir}
+binaries="cptron"
+os="windows 
+linux 
+freebsd 
+openbsd 
+darwin 
+android 
+ios"
+
+cpu_arch="386
+amd64
+arm
+arm64
+loong64"
+for sys in $os; do
+    for arch in $cpu_arch; do
+        platstr=${sys}"_"${arch}
+        [ $sys == "ios" ] && continue
+        [ $sys == "android" ] && continue
+        [ $sys == "darwin" -a $arch != "amd64" -a $arch != "arm64" ] && continue
+        [ $arch == "loong64" -a $sys != "linux" ] && continue
+        export GOOS=$sys
+        export GOARCH=$arch
+        for bin in $binaries; do
+            file_name=$bin"_"$arch
+            [ $sys != "windows" ] && file_name=$file_name'_'$os
+            [ $sys == "windows" ] && file_name=$file_name'.exe'
+            mkdir -p $out_dir
+            CGO_ENABLED=0 go build -o $out_dir/$file_name cmd/$bin/main/main.go
+        done
+    done
+done
---
+++ .build/push-binaries.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+out_dir = $PWD/out
+version="staging"
+[ ${CODING_BRANCH} -neq "main" ] && version=${CODING_BRANCH}
+
+GENERIC_ARTIFACTS_MACHINE="visoft-generic.pkg.coding.net"
+GENERIC_ARTIFACTS_STORE="/imetnide/binary"
+GENERIC_ARTIFACTS_USER=binary-1746428262480
+GENERIC_ARTIFACTS_PWD=9dfcfc87722fdc68c7883d2f82c314ffb6135a93
+
+for binary in `ls ${out_dir}/`;
+do
+    curl -T ${binary} \
+        -u ${GENERIC_ARTIFACTS_USER}:${GENERIC_ARTIFACTS_PWD} \
+        "https://"${GENERIC_ARTIFACTS_MACHINE}${GENERIC_ARTIFACTS_STORE}"/"${binary}"?version="${version}
+done
