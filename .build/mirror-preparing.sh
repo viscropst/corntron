55673a10535e96b47ac317ab2b9818a2b1f67b4b
viscropst <viscropst@petalmail.com>
2025-08-17 17:10:07 +0800

    .cnb: fix the format of public key and bare url when syncing

--- .build/mirror-preparing.sh
+++ .build/mirror-preparing.sh
@@ -4,12 +4,12 @@ id_name=id_rsa
 [ ! -f /usr/bin/git ] && sed -i 's/dl-cdn.alpinelinux.org/mirrors.cloud.tencent.com/g' /etc/apk/repositories
 [ ! -f /usr/bin/git ] && apk add --no-cache --update git gpg less openssh patch perl base64
 [ ! -d ~/.ssh ] && mkdir ~/.ssh
-echo ${CI_PUBLIC_KEY} > ~/.ssh/${id_name}.pub
+echo "${CI_PUBLIC_KEY}" > ~/.ssh/${id_name}.pub
 echo public key was:
 cat ~/.ssh/${id_name}.pub
 chmod 600 ~/.ssh/${id_name}.pub
 
-echo ${CI_PRIVATE_KEY} | base64 -d > ~/.ssh/${id_name}
+echo "${CI_PRIVATE_KEY}" > ~/.ssh/${id_name}
 echo private key was:
 chmod 600 ~/.ssh/${id_name}
 
--- .build/mirror-to-github.sh
+++ .build/mirror-to-github.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 repowrok=$PWD/git
 echo Start cloning bare of code
-ssh-keyscan e.coding.net >> ~/.ssh/known_hosts
-ssh -T git@e.coding.net
-[ ! -d $repowrok ] && git clone --mirror git@e.coding.net:visoft/imetnide/cryphtron-prototype.git $repowrok
+#ssh-keyscan e.coding.net >> ~/.ssh/known_hosts
+#ssh -T git@e.coding.net
+[ ! -d $repowrok ] && git clone --mirror https://cnb.cool/viscropst/corntron $repowrok
 [ ! -d $repowrok ] && exit 1
 cd $repowrok
 echo Start push to Github
