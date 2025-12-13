92102ed84e1b0872ae917cb5316f8a013334544b
viscropst <viscropst@petalmail.com>
2025-12-14 04:47:06 +0800

    .build: 修复构建脚本字符串比较问题并优化发布流程

--- .build/build-binaries.sh
+++ .build/build-binaries.sh
@@ -4,32 +4,32 @@ go mod tidy
 out_dir=$PWD/out
 mkdir -p ${out_dir}
 binaries="corntron"
-os="windows 
-linux 
-freebsd 
-openbsd 
-darwin 
-android 
+os="windows \
+linux \
+freebsd \
+openbsd \
+darwin \
+android \
 ios"
 
-cpu_arch="386
-amd64
-arm
-arm64
+cpu_arch="386 \
+amd64 \
+arm \
+arm64 \
 loong64"
 for sys in $os; do
     for arch in $cpu_arch; do
         platstr=${sys}"_"${arch}
-        [ $sys == "ios" ] && continue
-        [ $sys == "android" ] && continue
-        [ $sys == "darwin" -a $arch != "amd64" -a $arch != "arm64" ] && continue
-        [ $arch == "loong64" -a $sys != "linux" ] && continue
+        [ "$sys" = "ios" ] && continue
+        [ "$sys" = "android" ] && continue
+        [ "$sys" = "darwin" -a "$arch" != "amd64" -a "$arch" != "arm64" ] && continue
+        [ "$arch" = "loong64" -a "$sys" != "linux" ] && continue
         export GOOS=$sys
         export GOARCH=$arch
         for bin in $binaries; do
             file_name=$bin"_"$arch
-            [ $sys != "windows" ] && file_name=$file_name'_'$sys
-            [ $sys == "windows" ] && file_name=$file_name'.exe'
+            [ "$sys" != "windows" ] && file_name=$file_name'_'$sys
+            [ "$sys" = "windows" ] && file_name=$file_name'.exe'
             mkdir -p $out_dir
             echo "output file:" $out_dir/$bin/$file_name
             echo "build binary:" $bin
--- .build/push-binaries-codeberg.sh
+++ .build/push-binaries-codeberg.sh
@@ -20,11 +20,11 @@ release_id=""
 echo "Getting release by tag"
 result=`curl -s -H "authorization: token "${CI_CB_TOKEN} \
     -H 'accept: application/json' \
-    "https://"${GENERIC_ARTIFACTS_MACHINE}${GENERIC_ARTIFACTS_STORE}"/tags/"${version}`
+    "https://"${GENERIC_ARTIFACTS_MACHINE}${CODEBERG_RELEASE_PATH}"/tags/"${version}`
 echo "After get release by tag"
 echo "result:" $result
 release_id=`echo $result | jq -r '.id'`
-if [ "${release_id}" != "" ]; then
+if [ "${release_id}" != "null" ]; then
     echo "Release already exists, deleting"
     curl -s -X DELETE \
         -H "accept: application/json" \
@@ -32,11 +32,13 @@ if [ "${release_id}" != "" ]; then
         "https://"${GENERIC_ARTIFACTS_MACHINE}${CODEBERG_RELEASE_PATH}"/"${release_id}
 fi
 echo "Creating the CodeBerg release, tagged:"${version}
+
+echo ${LATEST_CHANGELOG} > out/corntron/CHANGELOG.md
 release_id=`curl -s -X POST \
     -H 'accept: application/json' \
     -H "Content-Type: application/json" \
     -H "authorization: token "${CI_CB_TOKEN} \
-    -d '{"tag_name":"'${version}'","name":"'${version}' release","body":"this is a release by tag","draft":false,"prerelease":'${pre_release}'}' \
+    -d '{"tag_name":"'${version}'","name":"'${version}' release","body":"read changelog in CHANGELOG.md","draft":false,"prerelease":'${pre_release}'}' \
     "https://${GENERIC_ARTIFACTS_MACHINE}${CODEBERG_RELEASE_PATH}" | jq -r ".id"`
 
 for binary in `ls ${out_dir}/`;
@@ -50,19 +52,19 @@ do
         if [ "${release_id}" != "null" ]; then
             echo "Uploading to CodeBerg release"
             echo "https://"${GENERIC_ARTIFACTS_MACHINE}${CODEBERG_RELEASE_PATH}"/"${release_id}"/assets"
-            curl -X POST \
-                -F "attachment=@${file}" \
+            curl --max-time 120 -X POST \
                 -H "authorization: token "${CI_CB_TOKEN} \
-                "https://"${GENERIC_ARTIFACTS_MACHINE}${CODEBERG_RELEASE_PATH}"/"${release_id}"/assets"
+                "https://"${GENERIC_ARTIFACTS_MACHINE}${CODEBERG_RELEASE_PATH}"/"${release_id}"/assets" \
+                -F "attachment=@${file}"
         fi
         echo "Deleting from generic package store"
-        curl -s -X DELETE \
+        curl -s --max-time 120 -X DELETE \
             -H "authorization: token "${CI_CB_TOKEN} \
-            "https://"${GENERIC_ARTIFACTS_MACHINE}${GENERIC_ARTIFACTS_STORE}"/"${binary}"/"${version}"/"${filename} || true
-        echo "Uploading to generic package store"
-        echo "https://"${GENERIC_ARTIFACTS_MACHINE}${GENERIC_ARTIFACTS_STORE}"/"${binary}"/"${version}"/"${filename}
-        curl -T $file \
-            -H "authorization: token"${CI_CB_TOKEN} \
             "https://"${GENERIC_ARTIFACTS_MACHINE}${GENERIC_ARTIFACTS_STORE}"/"${binary}"/"${version}"/"${filename}
+        echo "\nUploading to generic package store"
+        echo "https://"${GENERIC_ARTIFACTS_MACHINE}${GENERIC_ARTIFACTS_STORE}"/"${binary}"/"${version}"/"${filename}
+        curl -H "authorization: token "${CI_CB_TOKEN} \
+            "https://"${GENERIC_ARTIFACTS_MACHINE}${GENERIC_ARTIFACTS_STORE}"/"${binary}"/"${version}"/"${filename} \
+            -T $file --max-time 120
     done
 done
